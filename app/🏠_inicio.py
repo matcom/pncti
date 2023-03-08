46ed6a62fd002decedf9dac8a898c4077102ea74
Alejandro Piad-Morffis <apiad@apiad.net>
2023-03-08 12:26:12 -0500

    Change version location

--- app/🏠_inicio.py
+++ app/🏠_inicio.py
@@ -5,11 +5,10 @@ import os
 
 st.set_page_config("Proyectos UH", page_icon="🏠", layout="wide")
 
+git_version = os.popen("git log -n 1 --pretty='%cd - %h'").readline().strip()
+st.info(f"ℹ️ Versión desplegada: **{git_version}**")
+
 user = auth.authenticate()
 
 with open("/src/data/inicio.md") as fp:
     st.write(fp.read().format(user=user, role=st.session_state.role, program=st.session_state.program))
-
-
-git_version = os.popen("git log -n 1 --pretty='%cd - %h'").readline().strip()
-st.info(f"ℹ️ Versión desplegada: **{git_version}**")
