4213001147a4fec30aa6c2020dcb8c732a09da26
Manuel Vilas <mvilasvaliente@gmail.com>
2023-03-14 21:53:12 -0400

    Add email feature and refactor expert assignment

--- app/pages/004_⚙️_programa.py
+++ app/pages/004_⚙️_programa.py
@@ -5,6 +5,7 @@ import auth
 
 from models import Application, Status
 from utils import show_app_state
+from tools import send_from_template
 
 
 st.set_page_config(page_title="Proyectos UH - Programa", page_icon="⚙️", layout="wide")
@@ -23,7 +24,9 @@ if st.session_state.role != "Dirección de Programa":
 applications = Application.load_from(program=st.session_state.program)
 
 df = []
-experts = yaml.safe_load(open("/src/data/roles.yml"))[st.session_state.program]['Experto']
+
+roles = yaml.safe_load(open("/src/data/roles.yml"))[st.session_state.program]
+experts = roles['Experto']
 
 if not applications:
     st.warning(
@@ -55,38 +58,65 @@ if app is None:
 
 left, right = show_app_state(app, expert=True)
 
+def assign_expert(app: Application, i: int, tab):
+    "Asignar experto"
+
+    value = tab.selectbox(label="Expertos", options=[f"{name} ({email})" for email, name in experts.items() 
+                                                    if email != app.expert_1 and email != app.expert_2],
+                         key=f"sb_expert{i}")
+
+    def assign_expert(app, value):
+        setattr(app, f'expert_{i}', value.split("(")[-1][:-1])
+
+        app.save()
+
+    tab.button("Asignar experto", on_click=assign_expert, args=(app, value), key=f"b_expert{i}")
+    
+def unassign_expert(app: Application, i: int):
+    "Quitar asignación"
+    
+    setattr(app, f"expert_{i}", None)
+    app.save()
+
 with right:
     st.write(f"#### Evaluación de los expertos")
     
     anexo = config["programs"][app.program]["project_types"][app.project_type]
     name = config["docs"][anexo]["name"]
     file_name = config["docs"][anexo]["file_name"]
+    
+    tabs = st.tabs(["Experto 1", "Experto 2"])
+    for i, tab in enumerate(tabs):
         
-    for i in range(1, 3):
-        exp = getattr(app, f"expert_{i}")
-        st.write(f"**Experto {i}:** {experts[exp] if exp in experts.keys() else 'No está asignado'}")
-        
-        exp_file = app.file(file_name=file_name, expert=exp)
-        if exp_file:
-            st.download_button(
-                f"⏬ Descargar última versión subida del {name}", exp_file, file_name=file_name
-            )
+        exp = getattr(app, f"expert_{i+1}")
+        if exp not in experts.keys():
+            tab.warning("No está asignado", icon="⚠️")
+            assign_expert(app, i+1, tab)
+           
         else:
-            st.warning("No hay evaluación de este experto", icon="⚠️")
+            tab.write(f"**Nombre:** {experts[exp]}")
         
-
-def assign_expert(app: Application):
-    "Asignar experto"
-
-    value = st.multiselect(label="Expertos", options=[f"{name} ({email})" for email, name in experts.items()], max_selections=2)
-
-    def assign_expert(app, value):
-        for i, expert in enumerate(value):
-            setattr(app, f'expert_{i+1}', str(expert).split("(")[-1][:-1])
-
-        app.save()
-
-    st.button("Asignar expertos", on_click=assign_expert, args=(app, value))
+            exp_file = app.file(file_name=file_name, expert=exp)
+            if exp_file:
+                tab.download_button(
+                    f"⏬ Descargar última versión subida del {name}", exp_file, file_name=file_name
+                )
+            else:
+                tab.warning("No hay evaluación de este experto", icon="⚠️")
+                
+            with tab.expander("Enviar correo"):
+                email = st.form(key=f"expert_email{i}", clear_on_submit=True)
+                email.caption(f"A: {exp}")
+                message = email.text_area("Mensaje")
+                attached = email.file_uploader("Adjuntar archivos", accept_multiple_files=True)
+                submited = email.form_submit_button(label="Enviar")
+                if submited:
+                    send_from_template("program", exp, 
+                                       message=message, 
+                                       program=st.session_state.program, 
+                                       user=roles["Dirección de Programa"][st.session_state.user])
+                    
+            tab.button(label="⛔ Quitar asignación", on_click=unassign_expert, args=[app, i+1], key=f"u_expert{i}")
 
 
 def review_docs(app: Application):
@@ -105,9 +135,9 @@ def review_docs(app: Application):
     st.button("Aplicar dictamen", on_click=review_doc, args=(app, value))
 
 
-actions = { func.__doc__: func for func in [review_docs, assign_expert]}
+actions = { func.__doc__: func for func in [review_docs]}
 
-with right:
+with left:
     st.write("#### Acciones")
 
     action = st.selectbox("Seleccione una opción", actions)
